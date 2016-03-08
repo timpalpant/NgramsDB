9e4423121f0ba34bd27f40d97d9f6794fd3d27ec
Timothy Palpant <tim@palpant.us>
2016-03-07 22:11:04 -0500

    add script to process all ngrams

--- process_ngrams.sh
+++ process_ngrams.sh
@@ -1,4 +1,5 @@
-time ls /Volumes/Data/google-ngrams/google-books-20120701/googlebooks-eng-all-2gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,2n -S 5% '|' ./collapse_dups '>' /Volumes/Data/google-ngrams/processed/{/.}.txt
-time ls /Volumes/Data/google-ngrams/google-books-20120701/googlebooks-eng-all-3gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,3n -S 5% '|' ./collapse_dups '>' /Volumes/Data/google-ngrams/processed/{/.}.txt
-time ls /Volumes/Data/google-ngrams/google-books-20120701/googlebooks-eng-all-4gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,4n -S 5% '|' ./collapse_dups '>' /Volumes/Data/google-ngrams/processed/{/.}.txt
-time ls /Volumes/Data/google-ngrams/google-books-20120701/googlebooks-eng-all-5gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,5n -S 5% '|' ./collapse_dups '>' /Volumes/Data/google-ngrams/processed/{/.}.txt
+mkdir -p processed
+time ls googlebooks-eng-all-2gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,2n -S 5% '|' ./collapse_dups '>' processed/{/.}.txt
+time ls googlebooks-eng-all-3gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,3n -S 5% '|' ./collapse_dups '>' processed/{/.}.txt
+time ls googlebooks-eng-all-4gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,4n -S 5% '|' ./collapse_dups '>' processed/{/.}.txt
+time ls googlebooks-eng-all-5gram-20120701-*.gz | parallel -j 6 time pigz -d -c {} '|' ./ngramsdb_prep -word_file=word.txt '|' sort -k1,5n -S 5% '|' ./collapse_dups '>' processed/{/.}.txt
